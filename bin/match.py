e008541074a782dc67a82c1fa1184c6c6e139323
Kévin Dérécusson <derec84@hotmail.fr>
2022-01-19 10:03:45 +0100

    Create a list of match results

--- bin/match.py
+++ bin/match.py
@@ -1,9 +1,40 @@
 """"Définit un match."""
 
 
-def result_match_notation():
-    """Permet d'attribuer un résultat pour chaque paires de joueur."""
-    pass
+def create_match_results(self):
+    """Créer une liste de score."""
+    i = 1
+    results = []
+    players = self
+    while players != []:
+        print(f'\nNotation match #{i} : '
+        f'{players[0].first_name} {players[0].last_name} vs '
+        f'{players[1].first_name} {players[1].last_name}')
+        while True:
+            try:
+                result_one = float(input(f'Résulat de {players[0].first_name} ' 
+                f'{players[0].last_name} : '
+                '(1 pour gagner, 0 perdu, 0.5 pour égalité) : '))
+                if result_one == 0:
+                    result_two = 1
+                    break
+                elif result_one == 1:
+                    result_two = 0
+                    break
+                elif result_one == 0.5:
+                    result_two = 0.5
+                    break
+                else:
+                    print('Veuillez inscrire un score de 0, 1 ou 0.5')
+            except ValueError:
+                print('Vous n\'avez pas inscrit un chiffre.')
+        results.append(result_one)
+        results.append(result_two)
+        players = players[2:]
+        i += 1
+    
+    return results
+
 
 def create_match():
     """Créer un match."""
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bin.matchmaking import matchmaking
 from bin.tournament import create_tournament
 from bin.player import create_player, select_random_player_in_database
 from bin.round import create_round
+from bin.match import create_match_results
 
 def main():
     database_players = []
@@ -25,6 +26,7 @@ def main():
             choice = 'O' #input(f'Terminer le tour {i+1} ? Oui = o')
             if choice.upper() == 'O':
                 round.ending_date = '{}'.format(time.strftime("%Y-%m-%d_%Hh"))
+                results = create_match_results(player_matchmaking)
 
 if __name__ == "__main__":
     main()
